2c32666c0f596479ef3e866332e65e53556236fd
Narek Ghevandiani <n.ghevandiani@gmail.com>
2018-09-30 16:36:06 +0400

    close modal button

--- src/components/action_item_modal/action_item_modal.js
+++ src/components/action_item_modal/action_item_modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import CloseIcon from '@material-ui/icons/Clear';
 import {closeModalAction} from 'actions/action_item';
 import NewActionItem from 'components/new_action_item';
 
@@ -18,6 +19,9 @@ class ActionItemModal extends React.Component {
       <div className="modal">
         <div className="background" onClick={closeModalAction}>
           <div className="content" onClick={this.contentClickHandler}>
+            <div className="close-button-wrapper">
+              <CloseIcon onClick={closeModalAction} />
+            </div>
             <NewActionItem />
           </div>
         </div>
